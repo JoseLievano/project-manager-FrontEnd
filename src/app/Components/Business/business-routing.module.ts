7062357df0eff63f2414b4954ec9b18f738a2689
Joseph <lievanosan@gmail.com>
2022-10-12 19:59:02 +0200

    Eliminate the routers arrays from business-routing.module.ts because they are not necessary anymore, because now, BsClientIndexComponent is declared inside the main App-routing Module as a child of "dashboard"

--- src/app/Components/Business/business-routing.module.ts
+++ src/app/Components/Business/business-routing.module.ts
@@ -1,23 +1,18 @@
 import {NgModule} from "@angular/core";
-import {RouterModule, Routes} from "@angular/router";
 import {BsClientIndexComponent} from "./bsClient/bs-client-index/bs-client-index.component";
 import {NgClass} from "@angular/common";
 import {AppRoutingModule} from "../../app-routing.module";
 import {SharedComponentsModule} from "../Shared/shared-components.module";
 
-const routes : Routes = [
-    {path: "bs_client", component: BsClientIndexComponent}
-  ]
 
 @NgModule({
   declarations: [BsClientIndexComponent],
   imports: [
-    RouterModule.forRoot(routes),
     NgClass,
     AppRoutingModule,
     SharedComponentsModule
   ],
-  exports: [RouterModule]
+  exports: []
 })
 
 export class BusinessRoutingModule{}
