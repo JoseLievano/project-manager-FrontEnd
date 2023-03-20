4d6ff6583f3c6b7a584a2fe63787b46d58c553dc
Joseph <lievanosan@gmail.com>
2023-03-20 15:12:33 +0100

    Modify getAlerts, addNewAlert and removeAlert methods inside alert.service.ts

--- src/app/Service/Shared/alert.service.ts
+++ src/app/Service/Shared/alert.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@angular/core';
+import {UiMessage} from "../../Model/Shared/ui-message";
+import {Observable, Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlertService {
 
+  private alerts : UiMessage[] = [];
 
+  private alertsChanged : Subject<UiMessage[]> = new Subject<UiMessage[]>();
 
-  constructor() { }
+  constructor() {  }
+
+  public getAlerts() : Observable<UiMessage[]>{
+    return this.alertsChanged.asObservable();
+  }
+
+  public addNewAlert(newAlert : UiMessage) : void {
+    this.alerts.unshift(newAlert);
+    this.alertsChanged.next(this.alerts);
+  }
+
+  public removeAlert(toRemove : UiMessage) : void {
+    let toRemoveIndex = this.alerts.indexOf(toRemove);
+    this.alerts.splice(toRemoveIndex, 1);
+    this.alertsChanged.next(this.alerts);
+  }
+
 }
