46e74fe5944e62b037feb2ac7334921a86d88374
Joseph <lievanosan@gmail.com>
2023-03-23 08:36:49 +0100

    Add the methods getSideBarmenuElements, businessIsLoaded and unloadBusiness to the sidebar-menu.service.ts

--- src/app/Service/Shared/sidebar-menu.service.ts
+++ src/app/Service/Shared/sidebar-menu.service.ts
@@ -3,6 +3,8 @@ import {LoginService} from "./login.service";
 import {SidebarMenuElement} from "../../Model/Shared/SidebarMenuElement";
 import {SidebarSubMenuElement} from "../../Model/Shared/SidebarSubMenuElement";
 import {User} from "../../Model/Shared/User";
+import {userRole} from "../../Constant/userRole";
+import {BusinessService} from "../Business/business.service";
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,7 @@ export class SidebarMenuService {
 
   private sidebarIsVisible : Boolean = true;
 
-  public actualUser : User;
+  public actualUser : User | null;
 
   private mainMenu : SidebarMenuElement[] = [
     SidebarMenuElement.builder()
@@ -32,24 +34,24 @@ export class SidebarMenuService {
     SidebarMenuElement.builder()
       .setName("Dashboard")
       .setPath("dashboard")
-      .setCanActiveRoles(["ROLE_CLIENT", "ROLE_ADMIN", "ROLE_EMPLOYEE"]),
+      .setNeedBusinessLoaded(false)
+      .setCanActiveRoles([userRole.ADMIN, userRole.CLIENT, userRole.BS_CLIENT, userRole.BS_EMPLOYEE, userRole.BS_MANAGER]),
     SidebarMenuElement.builder()
       .setName("Business")
       .setPath("business")
       .setNeedBusinessLoaded(false)
       .setShowWithBusinessLoaded(false)
-      .setCanActiveRoles(["ROLE_CLIENT", "ROLE_ADMIN", "ROLE_EMPLOYEE"]),
+      .setCanActiveRoles([userRole.ADMIN, userRole.CLIENT, userRole.EMPLOYEE]),
     SidebarMenuElement.builder()
       .setName("No Business")
       .setPath("#")
       .setNeedBusinessLoaded(false)
       .setShowWithBusinessLoaded(false)
-      .setCanActiveRoles(["ROLE_CLIENT", "ROLE_ADMIN"]),
+      .setCanActiveRoles([userRole.ADMIN, userRole.CLIENT]),
     SidebarMenuElement.builder()
       .setName("Projects")
       .setPath("#")
-      .setNeedBusinessLoaded(true)
-      .setCanActiveRoles(["ROLE_CLIENT", "ROLE_ADMIN"])
+      .setCanActiveRoles([userRole.ADMIN, userRole.CLIENT, userRole.BS_MANAGER, userRole.BS_EMPLOYEE])
   ]
 
   private businessMenu : SidebarMenuElement[] = [
@@ -70,8 +72,8 @@ export class SidebarMenuService {
     ]
   }
 
-  constructor(private loginService : LoginService) {
-
+  constructor(private loginService : LoginService, private businessService : BusinessService) {
+    this.actualUser = loginService.getActualUser();
   }
 
   toggleSidebar(){
@@ -86,8 +88,48 @@ export class SidebarMenuService {
 
     let actualSidebar : SidebarMenuElement [] = [];
 
-    return actualSidebar;
+    let actualRole : String = "";
 
+    if (this.actualUser?.roles){
+      actualRole = this.actualUser.roles[0];
+    }else {
+      return actualSidebar;
+    }
+
+    let businessLoaded : Boolean = false;
+    if (this.businessService.getLoadedBusiness() >= 0){
+      businessLoaded = true;
+    }
+
+    for (let item of this.mainMenu2){
+
+      let userRoleIndex = item.canActiveRoles.indexOf(actualRole);
+
+      if (userRoleIndex >= 0 && businessLoaded == item.showWithBusinessLoaded){
+
+        if ((item.needBusinessLoaded && businessLoaded) || !item.needBusinessLoaded){
+          actualSidebar.push(item);
+        }
+      }
+    }
+    return actualSidebar;
+  }
+
+  businessIsLoaded() : Boolean{
+    return this.businessService.getLoadedBusiness() >= 0;
+  }
+
+  unloadBusiness() : void {
+    let roles : String[] = [
+      userRole.ADMIN,
+      userRole.CLIENT
+    ]
+
+    if (this.actualUser?.roles){
+      if (roles.indexOf(this.actualUser.roles[0]) >= 0){
+        this.businessService.unloadBusiness();
+      }
+    }
   }
 
 }
