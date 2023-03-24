3a9b50faed36362fe96802f9213f1da0a669b6bf
Joseph <lievanosan@gmail.com>
2023-03-24 10:10:06 +0100

    New constant that holds the name and paths of the different pages in the APP

---
+++ src/app/Constant/paths.ts
@@ -0,0 +1,90 @@
+export const Paths = {
+  DASHBOARD : {
+    name : "Dashboard",
+    path : "dashboard"
+  },
+  BUSINESS: {
+    name : "Business",
+    path : "business"
+  },
+  BS_CLIENT: {
+    name : "Client",
+    path : "bs_client"
+  },
+  BS_MANAGER: {
+    name : "Manager",
+    path : "bs_manager"
+  },
+  BS_EMPLOYEE: {
+    name : "Employee",
+    path : "bs_employee"
+  },
+  BS_STATUS: {
+    name : "Status",
+    path : "bs_status"
+  },
+  BS_PRIORITY: {
+    name : "Priority",
+    path : "bs_priority"
+  },
+  BS_TYPE: {
+    name : "Type",
+    path : "bs_type"
+  },
+  BS_TASK_CATEGORY: {
+    name : "Task Category",
+    path : "bs_task_category"
+  },
+  BS_DOCS_CATEGORY: {
+    name : "Docs Category",
+    path : "bs_docs_category"
+  },
+  BS_DOC: {
+    name : "Doc",
+    path : "bs_doc"
+  },
+  BS_KB_CATEGORY: {
+    name : "KB Category",
+    path : "bs_kb_category"
+  },
+  BS_KB: {
+    name : "KB",
+    path : "bs_kb"
+  },
+  BS_INVOICE: {
+    name : "Invoice",
+    path : "bs_invoice"
+  },
+  BS_PROJECT: {
+    name : "Project",
+    path : "bs_project"
+  },
+  BS_PR_TASK: {
+    name : "Task",
+    path : "bs_pr_task"
+  },
+  BS_PR_CHANNEL: {
+    name : "Channel",
+    path : "bs_pr_channel"
+  },
+  BS_PR_COMMENT: {
+    name : "Comment",
+    path : "bs_pr_comment"
+  },
+  BS_PR_DOCS_CATEGORY: {
+    name : "Docs Category",
+    path : "bs_pr_docs_category"
+  },
+  BS_PR_DOCS: {
+    name : "Doc",
+    path : "bs_pr_docs"
+  },
+  BS_PR_KB_CATEGORY: {
+    name : "KB Category",
+    path : "bs_pr_kb_category"
+  },
+  BS_PR_KB: {
+    name : "KB",
+    path : "bs_pr_kb"
+  }
+}
