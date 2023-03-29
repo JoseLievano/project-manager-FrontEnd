6d4305648b984b1ad8cdd7a6342ae2bd1e670efd
Joseph <lievanosan@gmail.com>
2023-03-29 17:00:04 +0200

    Pass the url the service ModelService needs to use.

--- src/app/Service/Business/business.service.ts
+++ src/app/Service/Business/business.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Const} from "../../Constant/const";
 import {HttpClient} from "@angular/common/http";
 import {ModelService} from "../Shared/model.service";
@@ -13,16 +13,16 @@ import {Router} from "@angular/router";
 @Injectable({
   providedIn: 'root'
 })
-export class BusinessService extends ModelService<Business>{
+export class BusinessService extends ModelService<Business> {
 
-  private url : String;
+  private url: String;
 
-  protected override actualUser : User | null;
+  protected override actualUser: User | null;
 
-  constructor(protected override http : HttpClient,
-              private loginService : LoginService,
-              private router : Router) {
-    super(http, loginService.getActualUser());
+  constructor(protected override http: HttpClient,
+              private loginService: LoginService,
+              private router: Router) {
+    super(http, loginService.getActualUser(), Const.API_URL + Const.BUSINESS);
     this.url = Const.API_URL + Const.BUSINESS;
   }
 
@@ -31,27 +31,27 @@ export class BusinessService extends ModelService<Business>{
     if (this.actualUser != null || this.actualUser != undefined) {
       // @ts-ignore
       if (this.actualUser.roles[0] != "ROLE_CLIENT") {
-        throw {message : "You don't have permission to view this"};
+        throw {message: "You don't have permission to view this"};
       }
     }
 
-    if (pageRequest.sort?.length == 0){
+    if (pageRequest.sort?.length == 0) {
       pageRequest.sort = [
-        {"property" : "id", "isAscending": true}
+        {"property": "id", "isAscending": true}
       ];
     }
   }
 
   override getButtonPermissions(): ActionsButtons[] {
-    let actions : ActionsButtons[] = [
+    let actions: ActionsButtons[] = [
       {actionName: tableActionButton.LOAD, roles: ["ROLE_CLIENT"]},
       {actionName: tableActionButton.EDIT, roles: ["ROLE_CLIENT"]}
     ]
     return actions;
   }
 
-  override executeAction(action : string, id : number) : void {
-    switch (action){
+  override executeAction(action: string, id: number): void {
+    switch (action) {
       case tableActionButton.LOAD:
         this.loadBusiness(id);
         break;
@@ -59,20 +59,20 @@ export class BusinessService extends ModelService<Business>{
   }
 
   //Methods to load and unload a business in the local storage
-  public loadBusiness(businessID : number){
+  public loadBusiness(businessID: number) {
     window.localStorage.setItem("business", JSON.stringify(businessID));
     this.router.navigate(["dashboard"])
   }
 
-  public unloadBusiness(){
+  public unloadBusiness() {
     window.localStorage.setItem("business", "-1");
     this.router.navigate(["dashboard"]);
   }
 
-  public getLoadedBusiness() : number{
-    let actualBusiness : number = JSON.parse(<string> window.localStorage.getItem("business"));
+  public getLoadedBusiness(): number {
+    let actualBusiness: number = JSON.parse(<string>window.localStorage.getItem("business"));
 
-    if (actualBusiness >= 0){
+    if (actualBusiness >= 0) {
       return actualBusiness;
     }
     return -1;
