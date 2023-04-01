82c4ca66a41e8667e3f1e7f9b3b55456560bcac9
Joseph <lievanosan@gmail.com>
2023-04-01 11:02:29 +0200

    Modify imports from NgModule

--- src/app/Components/Features/Business/business-features.module.ts
+++ src/app/Components/Features/Business/business-features.module.ts
@@ -1,7 +1,6 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {BusinessComponentsModule} from "./business/business-components.module";
-
 
 
 @NgModule({
