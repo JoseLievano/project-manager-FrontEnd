29256e7abbe57600eb72ccbbbc5c138e035ca917
Joseph <lievanosan@gmail.com>
2023-04-30 19:52:23 +0200

    Add Category class with properties and methods in category.ts

---
+++ src/app/Model/Shared/category.ts
@@ -0,0 +1,22 @@
+import {CatContent} from "./cat-content";
+
+export class Category{
+
+  public id : number | null;
+
+  public name : string;
+
+  public description : string;
+
+  public icon : string;
+
+  public content : CatContent[];
+
+  public constructor() {
+  }
+
+  public setContent() : void {
+
+  }
+
+}
