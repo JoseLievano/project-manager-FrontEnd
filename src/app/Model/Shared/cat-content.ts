d68781e928dedc08bb4da22010e4d3612585da3f
Joseph <lievanosan@gmail.com>
2023-04-30 19:53:35 +0200

    feat: Add CatContent class with properties and methods in cat-content.ts

---
+++ src/app/Model/Shared/cat-content.ts
@@ -0,0 +1,17 @@
+import {Category} from "./category";
+
+export class CatContent{
+
+  public id : number | null;
+
+  public title : string;
+
+  public content : string;
+
+  public category : Category;
+
+  public setContent() : void {
+
+  }
+
+}
