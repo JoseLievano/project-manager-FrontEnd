37cfb2e27455c1e34926bfaef9cc83194de6de86
Joseph <lievanosan@gmail.com>
2023-05-01 22:42:24 +0200

    feat: Enhance BusinessService with roles, view keys and add new functionality

--- src/app/Service/Business/business.service.ts
+++ src/app/Service/Business/business.service.ts
@@ -11,6 +11,7 @@ import {Router} from "@angular/router";
 import {userRole} from "../../Constant/userRole";
 import {ErrorHandlerService} from "../Shared/error-handler.service";
 import {errorMessages} from "../../Constant/errorMessages";
+import {ViewKey, ViewKeyBuilder} from "../../Model/Shared/ViewKey";
 
 @Injectable({
   providedIn: 'root'
@@ -82,4 +83,43 @@ export class BusinessService extends ModelService<Business> {
     return -1;
   }
 
+  protected override rolesAbleToAddNew(): string[] {
+
+    return [userRole.CLIENT, userRole.ADMIN];
+
+  }
+
+
+  protected override getViewKeys(): ViewKey[] {
+
+    let commonRoles : string[] = [userRole.ADMIN, userRole.CLIENT, userRole.EMPLOYEE];
+
+    return [
+      ViewKeyBuilder.builder().setPrivateKeyName("id").setPublicKeyName("ID").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("name").setPublicKeyName("Name").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("taxID").setPublicKeyName("Tax ID").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("dateCreated").setPublicKeyName("Date").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("pendingInvoice").setPublicKeyName("Pending Invoice").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("overDue").setPublicKeyName("Overdue").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("isActive").setPublicKeyName("Active").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("client").setPublicKeyName("Client").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("invoices").setPublicKeyName("Invoices").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("plan").setPublicKeyName("Plan").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("bsClients").setPublicKeyName("Clients").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("bsDocs").setPublicKeyName("Docs").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("bsDocsCategories").setPublicKeyName("Docs Categories").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("bsEmployees").setPublicKeyName("Employees").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("bsInvoices").setPublicKeyName("Customer Invoices").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("bsKBCategories").setPublicKeyName("KB Categories").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("bsKBs").setPublicKeyName("KBs").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("bsManagers").setPublicKeyName("Managers").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("bsPriorities").setPublicKeyName("Priorities").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("bsStatuses").setPublicKeyName("Statuses").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("bsTaskCategories").setPublicKeyName("Task Categories").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("bsTypes").setPublicKeyName("Types").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("bsProjects").setPublicKeyName("Projects").setAccessRole(commonRoles),
+      ViewKeyBuilder.builder().setPrivateKeyName("bsPrTasks").setPublicKeyName("Tasks").setAccessRole(commonRoles)
+    ];
+
+  }
 }
