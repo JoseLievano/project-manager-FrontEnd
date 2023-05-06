737f188e3b563632bfa9816a01acfec82dade583
Joseph <lievanosan@gmail.com>
2023-05-06 23:14:17 +0200

    refactor: Update BsDocListComponent to use bsDocsCategory and add category service

--- src/app/Components/Features/Business/bsDoc/bs-doc-list/bs-doc-list.component.ts
+++ src/app/Components/Features/Business/bsDoc/bs-doc-list/bs-doc-list.component.ts
@@ -3,6 +3,8 @@ import {bsDoc} from "../../../../../Model/Business/bsDoc";
 import {User} from "../../../../../Model/Shared/User";
 import {BsDocService} from "../../../../../Service/Business/bs-doc.service";
 import {LoginService} from "../../../../../Service/Shared/login.service";
+import {bsDocsCategory} from "../../../../../Model/Business/bsDocsCategory";
+import {BsDocsCategoryService} from "../../../../../Service/Business/bs-docs-category.service";
 
 @Component({
   selector: 'app-bs-doc-list',
@@ -11,11 +13,11 @@ import {LoginService} from "../../../../../Service/Shared/login.service";
 })
 export class BsDocListComponent implements OnInit {
 
-  public model : bsDoc = new bsDoc();
+  public model : bsDocsCategory = new bsDocsCategory();
 
   private user : User | null;
 
-  constructor(public bsDocService : BsDocService,
+  constructor(public bsDocsCategoryService : BsDocsCategoryService,
               private loginService : LoginService) {
     this.user = loginService.getActualUser();
   }
@@ -23,4 +25,6 @@ export class BsDocListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  protected readonly bsDoc = bsDoc;
+  protected readonly bsDocsCategory = bsDocsCategory;
 }
