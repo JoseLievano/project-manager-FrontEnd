cfe47bb08382e701b4bb609ce42ae28c64584ccc
Joseph <lievanosan@gmail.com>
2023-05-16 13:03:36 +0200

    New component that will hold the form to create new bsDocsCategory.

---
+++ src/app/Components/Features/Business/bsDocCategory/add-new-bs-doc-category/add-new-bs-doc-category.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-bs-doc-category',
+  templateUrl: './add-new-bs-doc-category.component.html',
+  styleUrls: ['./add-new-bs-doc-category.component.css']
+})
+export class AddNewBsDocCategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
