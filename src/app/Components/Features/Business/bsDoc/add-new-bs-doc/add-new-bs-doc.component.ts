2f94677cffb218c7e3eb8a2b14cc8d749e022e9d
Joseph <lievanosan@gmail.com>
2023-05-16 13:03:11 +0200

    New component that will hold the form to create new bsDocs.

---
+++ src/app/Components/Features/Business/bsDoc/add-new-bs-doc/add-new-bs-doc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-bs-doc',
+  templateUrl: './add-new-bs-doc.component.html',
+  styleUrls: ['./add-new-bs-doc.component.css']
+})
+export class AddNewBsDocComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
