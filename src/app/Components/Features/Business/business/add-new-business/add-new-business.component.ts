8c704da02c5a08de0f4e1cdcfab5cd8c95df288c
Joseph <lievanosan@gmail.com>
2023-05-18 23:47:19 +0200

    New component that will hold the form to create new Business entities.

---
+++ src/app/Components/Features/Business/business/add-new-business/add-new-business.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-business',
+  templateUrl: './add-new-business.component.html',
+  styleUrls: ['./add-new-business.component.css']
+})
+export class AddNewBusinessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
