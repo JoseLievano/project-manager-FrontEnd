5f1b1bf64902c884a2fec225406ea574ba7c2473
Joseph <lievanosan@gmail.com>
2023-05-18 23:48:14 +0200

    New component that will be the first page to see bsDocsCategories

---
+++ src/app/Components/Features/Business/bsDocCategory/bs-doc-category/bs-doc-category.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bs-doc-category',
+  templateUrl: './bs-doc-category.component.html',
+  styleUrls: ['./bs-doc-category.component.css']
+})
+export class BsDocCategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
