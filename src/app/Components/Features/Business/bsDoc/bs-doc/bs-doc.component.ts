51188ab31820fa76d0b73f1b305b1d708c1f45f3
Joseph <lievanosan@gmail.com>
2023-05-18 23:47:50 +0200

    New component that will be the first page to see bsDocs

---
+++ src/app/Components/Features/Business/bsDoc/bs-doc/bs-doc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bs-doc',
+  templateUrl: './bs-doc.component.html',
+  styleUrls: ['./bs-doc.component.css']
+})
+export class BsDocComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
