953fb0288777e5b8fdd90e9eccf2eaf3b76fd04c
Joseph <lievanosan@gmail.com>
2023-06-02 00:09:08 +0200

    Create createInstance method. Add new actions buttons to getButtonPermissions.

--- src/app/Components/Features/Business/bsDoc/bs-doc-components.module.ts
+++ src/app/Components/Features/Business/bsDoc/bs-doc-components.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SharedComponentsModule} from "../../../Shared/shared-components.module";
 import { BsDocListComponent } from './bs-doc-list/bs-doc-list.component';
+import { AddNewBsDocComponent } from './add-new-bs-doc/add-new-bs-doc.component';
+import {RouterOutlet} from "@angular/router";
+import { BsDocComponent } from './bs-doc/bs-doc.component';
+import {ContentModule} from "../../../Shared/content/content.module";
 @NgModule({
   declarations: [
-    BsDocListComponent
+    BsDocListComponent,
+    AddNewBsDocComponent,
+    BsDocComponent
   ],
-  imports: [
-    CommonModule,
-    SharedComponentsModule
-  ]
+    imports: [
+        CommonModule,
+        SharedComponentsModule,
+        RouterOutlet,
+        ContentModule
+    ]
 })
 export class BsDocComponentsModule { }
