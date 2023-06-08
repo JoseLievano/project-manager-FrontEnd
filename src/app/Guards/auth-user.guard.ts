d70332b8b14017ead7a21b851fd68814623779e1
Joseph <lievanosan@gmail.com>
2023-06-08 17:33:45 +0200

    Changes to be compatible with the Angular version

--- src/app/Guards/auth-user.guard.ts
+++ src/app/Guards/auth-user.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import {LoginService} from "../Service/Shared/login.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthUserGuard implements CanActivate {
+export class AuthUserGuard  {
 
   constructor(private loginService : LoginService, private router : Router) {
   }
