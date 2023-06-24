34a9b9c23cfc0cc8b0567c1f73c164a95641cf61
Joseph <lievanosan@gmail.com>
2023-06-24 16:54:45 +0200

    Component to view Docs or KBs

---
+++ src/app/Components/Features/Business/bsDoc/view-doc/view-doc.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {id} from "@swimlane/ngx-datatable";
+import {ModelService} from "../../../../../Service/Shared/model.service";
+import {bsDoc} from "../../../../../Model/Business/bsDoc";
+import {BsDocService} from "../../../../../Service/Business/bs-doc.service";
+
+@Component({
+  selector: 'app-view-doc',
+  templateUrl: './view-doc.component.html',
+  styleUrls: ['./view-doc.component.css']
+})
+export class ViewDocComponent implements OnInit{
+
+  public id : number;
+
+  public docsModel : bsDoc = new bsDoc();
+
+  constructor(
+    private activatedRoute : ActivatedRoute,
+    public docsService : BsDocService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+
+    this.activatedRoute.params.subscribe(
+      params => {
+        this.id = params['id'];
+      }
+    )
+  }
+
+}
