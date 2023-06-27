64a963cb8ff14331d3f41deb2220ef7c91acd882
Joseph <lievanosan@gmail.com>
2023-06-27 17:02:23 +0200

    Component to hold bsKBs components

---
+++ src/app/Components/Features/Business/bsKB/bs-kb/bs-kb.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bs-kb',
+  templateUrl: './bs-kb.component.html',
+  styleUrls: ['./bs-kb.component.css']
+})
+export class BsKbComponent {
+
+}
