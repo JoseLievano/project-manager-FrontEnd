b6c893a60130d2a9cb6d61fd89096fc47ea05fd0
Joseph <lievanosan@gmail.com>
2023-06-29 00:50:08 +0200

    Modify bsStatus to use the new shape for models.

--- src/app/Model/Business/bsStatus.ts
+++ src/app/Model/Business/bsStatus.ts
@@ -3,61 +3,33 @@ import {bsPrTask} from "../Project/bsPrTask";
 
 export class bsStatus{
 
-  public id : Number | undefined | null;
+  public id : number | null;
 
-  public name : String | undefined | null;
+  public name : string | null;
 
-  public color : String | undefined | null;
+  public color : string | null;
 
-  public business : Business | undefined | null;
+  public business : Business | number | null;
 
-  public tasks : bsPrTask[] | undefined | null;
+  public tasks : bsPrTask[] | number | null;
 
   public bsStatus(
-                  id : Number | undefined | null,
-                  name : String | undefined | null,
-                  color : String | undefined | null,
-                  business : Business | undefined | null,
-                  tasks : bsPrTask[] | undefined | null
+    id? : number | undefined | null,
+    name? : string | undefined | null,
+    color? : string | undefined | null,
+    business? : Business | number | null,
+    tasks? : bsPrTask[] | number | null
   ){
-    this.id = id;
-    this.name = name;
-    this.color = color;
-    this.business = business;
-    this.tasks = tasks;
-  }
-
-  static builder() : bsStatus{
-    return new bsStatus();
-  }
-
-  public setId(id: Number) : bsStatus{
-    this.id = id;
-    return this;
-  }
-
-  public setName(name: String) : bsStatus{
-    this.name = name;
-    return this;
-  }
-
-  public setColor(color: String) : bsStatus{
-    this.color = color;
-    return this;
-  }
-
-  public setBusiness(business: Business) : bsStatus{
-    this.business = business;
-    return this;
-  }
-
-  public setTasks(tasks : bsPrTask[]) : bsStatus{
-    this.tasks = tasks;
-    return this;
-  }
-
-  public build() : bsStatus{
-    return this;
+    if (id)
+      this.id = id;
+    if (name)
+      this.name = name;
+    if (color)
+      this.color = color;
+    if (business)
+      this.business = business;
+    if (tasks)
+      this.tasks = tasks;
   }
 
 }
