84b40c3b7ff51af193468d357d1a9bc92e4c2394
Joseph <lievanosan@gmail.com>
2023-06-28 18:39:48 +0200

    Implements executeAction method.

--- src/app/Service/Business/bs-doc.service.ts
+++ src/app/Service/Business/bs-doc.service.ts
@@ -16,10 +16,12 @@ import {ViewKey, ViewKeyBuilder} from "../../Model/Shared/ViewKey";
 })
 export class BsDocService extends ModelService<bsDoc>{
 
-  private url : String;
-  constructor(protected override http : HttpClient,
-              protected override loginService : LoginService,
-              protected override router : Router) {
+  private url : string;
+  constructor(
+    protected override http : HttpClient,
+    protected override loginService : LoginService,
+    protected override router : Router
+  ) {
     super(http, loginService, Const.API_URL + Const.bs_DOC, router);
     this.url = Const.API_URL + Const.bs_DOC;
   }
@@ -34,7 +36,7 @@ export class BsDocService extends ModelService<bsDoc>{
     )
   }
 
-  override getButtonPermissions(): ActionsButtons[] {
+  public override getButtonPermissions(): ActionsButtons[] {
     return [
       {actionName: tableActionButton.VIEW, roles: [userRole.CLIENT]},
       {actionName: tableActionButton.EDIT, roles: [userRole.CLIENT]},
@@ -44,7 +46,7 @@ export class BsDocService extends ModelService<bsDoc>{
     ];
   }
 
-  override hiddenKeys(): HiddenKey[] {
+  public override hiddenKeys(): HiddenKey[] {
     return [
       new HiddenKey("content"),
     ]
@@ -67,6 +69,16 @@ export class BsDocService extends ModelService<bsDoc>{
       ViewKeyBuilder.builder().setPrivateKeyName("content").setPublicKeyName("Content").setAccessRole([userRole.BS_MANAGER]),
       ViewKeyBuilder.builder().setPrivateKeyName("business").setPublicKeyName("Business").setAccessRole(commonRoles)
     ]
-
   }
+
+  override executeAction(action: string, id: number) {
+    switch (action) {
+      case tableActionButton.DELETE: {
+        console.log("Deleting " + id);
+        this.deleteOne(id).subscribe();
+        break;
+      }
+    }
+  }
+
 }
