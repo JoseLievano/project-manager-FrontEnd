bcb571dd9f4b3f84e9224c15b5b5a636231dfd82
Joseph <lievanosan@gmail.com>
2023-07-01 00:51:48 +0200

    Add additional components.

--- src/app/Components/Shared/content/content.module.ts
+++ src/app/Components/Shared/content/content.module.ts
@@ -3,18 +3,28 @@ import { CommonModule } from '@angular/common';
 import { EditorComponent } from './editor/editor.component';
 import { ContentAddNewComponent } from './content-add-new/content-add-new.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import { ViewContentComponent } from './view-content/view-content.component';
+import { RenderParagraphComponent } from './render-blocks/render-paragraph/render-paragraph.component';
+import { RenderHeaderComponent } from './render-blocks/render-header/render-header.component';
+import { ContentEditComponent } from './content-edit/content-edit.component';
 
 
 
 @NgModule({
   declarations: [
     EditorComponent,
-    ContentAddNewComponent
+    ContentAddNewComponent,
+    ViewContentComponent,
+    RenderParagraphComponent,
+    RenderHeaderComponent,
+    ContentEditComponent
   ],
-  exports: [
-    EditorComponent,
-    ContentAddNewComponent
-  ],
+    exports: [
+        EditorComponent,
+        ContentAddNewComponent,
+        ViewContentComponent,
+        ContentEditComponent
+    ],
   imports: [
     CommonModule,
     ReactiveFormsModule
