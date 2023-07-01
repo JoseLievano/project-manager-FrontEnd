bd2a3b02a8c4a3da8341f0723a034252b1bdc4ba
Joseph <lievanosan@gmail.com>
2023-07-02 00:45:24 +0200

    Component to add new bs task categories.

---
+++ src/app/Components/Features/Business/bsTaskCategory/add-new-bs-task-category/add-new-bs-task-category.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import {BsTaskCategoryService} from "../../../../../Service/Business/bs-task-category.service";
+import {BusinessService} from "../../../../../Service/Business/business.service";
+import {bsTaskCategory} from "../../../../../Model/Business/bsTaskCategory";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AlertService} from "../../../../../Service/Shared/alert.service";
+import {UiMessage} from "../../../../../Model/Shared/ui-message";
+import {messageType} from "../../../../../Constant/messageType";
+
+@Component({
+  selector: 'app-add-new-bs-task-category',
+  templateUrl: './add-new-bs-task-category.component.html',
+  styleUrls: ['./add-new-bs-task-category.component.css']
+})
+export class AddNewBsTaskCategoryComponent {
+
+  private taskCategory : bsTaskCategory = new bsTaskCategory();
+
+  public newTaskCategoryForm : FormGroup = new FormGroup<any>({
+    name : new FormControl('', {validators : [Validators.required]})
+  })
+
+  constructor(
+    private bsTaskCategoryService : BsTaskCategoryService,
+    private businessService : BusinessService,
+    private alertService : AlertService
+  ) {
+    this.taskCategory.business = this.businessService.getLoadedBusiness();
+  }
+
+  public addNewTaskCategory(){
+
+    this.taskCategory.name = this.newTaskCategoryForm.value.name;
+    this.newTaskCategoryForm.controls['name'].setValue("");
+
+    this.bsTaskCategoryService.createNew<bsTaskCategory>(this.taskCategory).subscribe({
+      next : (response) => {
+        if (response.id)
+          this.bsTaskCategoryService.modelsChanged.emit(response.id);
+        this.alertService.addNewAlert(
+          new UiMessage("New task category " + response.name + " added", messageType.SUCCESS)
+        )
+      }
+    })
+  }
+
+}
