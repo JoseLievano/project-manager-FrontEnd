3d7b3974fe2ca09197f53c0f687bd2807d2d561a
Joseph <lievanosan@gmail.com>
2023-07-02 00:47:09 +0200

    Component to see a list of bs task categories.

---
+++ src/app/Components/Features/Business/bsTaskCategory/bs-task-category-list/bs-task-category-list.component.ts
@@ -0,0 +1,77 @@
+import {Component, OnInit} from '@angular/core';
+import {bsTaskCategory} from "../../../../../Model/Business/bsTaskCategory";
+import {PageRequest} from "../../../../../Model/Shared/pageRequest";
+import {BsTaskCategoryService} from "../../../../../Service/Business/bs-task-category.service";
+import {ErrorHandlerService} from "../../../../../Service/Shared/error-handler.service";
+import {AlertService} from "../../../../../Service/Shared/alert.service";
+import {UiMessage} from "../../../../../Model/Shared/ui-message";
+import {messageType} from "../../../../../Constant/messageType";
+
+@Component({
+  selector: 'app-bs-task-category-list',
+  templateUrl: './bs-task-category-list.component.html',
+  styleUrls: ['./bs-task-category-list.component.css']
+})
+export class BsTaskCategoryListComponent implements OnInit{
+
+  public taskCategories : bsTaskCategory[] = [];
+
+  private pageRequest : PageRequest = new PageRequest();
+
+  public isLoading : boolean = true;
+
+  constructor(
+    private bsTaskCategoryService : BsTaskCategoryService,
+    private errorService : ErrorHandlerService,
+    private alertService : AlertService
+  ) {
+      bsTaskCategoryService.modelsChanged.subscribe({
+      next : () => {
+        this.getTaskCategories();
+      }
+      });
+  }
+
+  ngOnInit(): void {
+    this.pageRequest.page = 0;
+    this.pageRequest.size = 50;
+    this.getTaskCategories();
+  }
+
+  private getTaskCategories(){
+    this.isLoading = true;
+    this.bsTaskCategoryService.getPageListView<bsTaskCategory>(this.pageRequest).subscribe({
+      next : (response) => {
+        if (response.content){
+          this.taskCategories = response.content;
+        }
+        this.isLoading = false;
+      },
+      error : err => {
+        this.errorService.processError(err);
+      }
+    })
+  }
+
+  public deleteTaskCategory(taskCategory : bsTaskCategory){
+    if (taskCategory.id){
+      this.bsTaskCategoryService.deleteOne(taskCategory.id).subscribe({
+        next : (response) => {
+          this.getTaskCategories();
+          this.alertService.addNewAlert(
+            new UiMessage("Task category " + response.name + " has been deleted", messageType.SUCCESS)
+          )
+        },
+        error : err => {
+          this.errorService.processError(err);
+        }
+      })
+    }else {
+      this.alertService.addNewAlert(
+        new UiMessage("Can't delete this task category")
+      )
+    }
+
+  }
+
+}
