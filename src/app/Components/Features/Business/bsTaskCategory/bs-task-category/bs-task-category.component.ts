e7d7029a9e4409fe0fc32471881612316a83df72
Joseph <lievanosan@gmail.com>
2023-07-02 00:45:55 +0200

    Base component for bsTaskCategories

---
+++ src/app/Components/Features/Business/bsTaskCategory/bs-task-category/bs-task-category.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bs-task-category',
+  templateUrl: './bs-task-category.component.html',
+  styleUrls: ['./bs-task-category.component.css']
+})
+export class BsTaskCategoryComponent {
+
+}
