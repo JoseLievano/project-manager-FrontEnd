4ec415b8a2249b16809fae973e0ea206284cd181
Joseph <lievanosan@gmail.com>
2023-07-02 00:46:39 +0200

    Service for bsTaskCategories

---
+++ src/app/Service/Business/bs-task-category.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {ModelService} from "../Shared/model.service";
+import {bsTaskCategory} from "../../Model/Business/bsTaskCategory";
+import {HttpClient} from "@angular/common/http";
+import {LoginService} from "../Shared/login.service";
+import {Router} from "@angular/router";
+import {Const} from "../../Constant/const";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BsTaskCategoryService extends ModelService<bsTaskCategory>{
+
+  private url : string;
+
+  constructor(
+    protected override http : HttpClient,
+    protected override loginService : LoginService,
+    protected override router : Router
+  ) {
+    super(http, loginService, Const.API_URL + Const.bs_TASK_CATEGORY, router)
+    this.url = Const.API_URL + Const.bs_TASK_CATEGORY;
+  }
+
+  createInstance(data : any): bsTaskCategory {
+    return new bsTaskCategory(
+      data.id,
+      data.name,
+      data.business,
+      data.tasks
+    );
+  }
+}
