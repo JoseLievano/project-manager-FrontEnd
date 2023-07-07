bc80da395adf99add512f65d426153622493d6e2
Joseph <lievanosan@gmail.com>
2023-07-07 18:56:12 +0200

    Component to edit a bsType

---
+++ src/app/Components/Features/Business/bsType/edit-bs-type/edit-bs-type.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-bs-type',
+  templateUrl: './edit-bs-type.component.html',
+  styleUrls: ['./edit-bs-type.component.css']
+})
+export class EditBsTypeComponent {
+
+}
