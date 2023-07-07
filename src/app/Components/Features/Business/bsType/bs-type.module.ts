f61e1cc97f101883d6494435fd77a7a2b7abdda2
Joseph <lievanosan@gmail.com>
2023-07-07 18:57:14 +0200

    bsType Module

---
+++ src/app/Components/Features/Business/bsType/bs-type.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddNewBsTypeComponent } from './add-new-bs-type/add-new-bs-type.component';
+import { BsTypeComponent } from './bs-type/bs-type.component';
+import { EditBsTypeComponent } from './edit-bs-type/edit-bs-type.component';
+import { BsTypeListComponent } from './bs-type-list/bs-type-list.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+
+
+@NgModule({
+  declarations: [
+    AddNewBsTypeComponent,
+    BsTypeComponent,
+    EditBsTypeComponent,
+    BsTypeListComponent
+  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule
+    ]
+})
+export class BsTypeModule { }
