785ff6aac2244230e72a453d52f8c73bccc63531
Joseph <lievanosan@gmail.com>
2023-07-07 18:57:30 +0200

    bsType main component

---
+++ src/app/Components/Features/Business/bsType/bs-type/bs-type.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bs-type',
+  templateUrl: './bs-type.component.html',
+  styleUrls: ['./bs-type.component.css']
+})
+export class BsTypeComponent {
+
+}
