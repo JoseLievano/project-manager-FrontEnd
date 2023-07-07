4a08529f9d32e8dddc742f5f994f1dbe0845e259
Joseph <lievanosan@gmail.com>
2023-07-08 01:01:43 +0200

    Add bsType field.

--- src/app/Model/Business/bsTaskCategory.ts
+++ src/app/Model/Business/bsTaskCategory.ts
@@ -1,5 +1,6 @@
 import {Business} from "./Business";
 import {bsPrTask} from "../Project/bsPrTask";
+import {bsType} from "./bsType";
 
 export class bsTaskCategory{
 
@@ -9,12 +10,15 @@ export class bsTaskCategory{
 
   public business : Business | number | null;
 
+  public types : bsType[] | number | null;
+
   public tasks : bsPrTask[] | number | null;
 
   public constructor(
     id? : number | null,
     name? : string | null,
     business? : Business | number | null,
+    types? : bsType[] | number | null,
     tasks? : bsPrTask[] | number | null
   ){
     if (id)
@@ -25,6 +29,9 @@ export class bsTaskCategory{
 
     if (business)
       this.business = business;
+
+    if (types)
+      this.types = types;
 
     if (tasks)
       this.tasks = tasks;
