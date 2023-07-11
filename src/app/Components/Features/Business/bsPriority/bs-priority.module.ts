75061f89cad3eb67cb12ac4c5c6bc1fcb24e4af4
Joseph <lievanosan@gmail.com>
2023-07-12 01:33:29 +0200

    Module to hols bsPriority component.

---
+++ src/app/Components/Features/Business/bsPriority/bs-priority.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BsPriorityComponent } from './bs-priority/bs-priority.component';
+
+
+
+@NgModule({
+  declarations: [
+    BsPriorityComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BsPriorityModule { }
