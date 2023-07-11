50c4115dfc93b5f607bd8597bd2d7bc75564cf0c
Joseph <lievanosan@gmail.com>
2023-07-11 22:33:37 +0200

    Creates addToeditorList method and implements logic for saveContent method.

--- src/app/Service/Shared/editor.service.ts
+++ src/app/Service/Shared/editor.service.ts
@@ -6,17 +6,43 @@ import EditorJS, {OutputData} from "@editorjs/editorjs";
 })
 export class EditorService {
 
-  private editorJS : EditorJS;
+  private editors : {id : number, editor : EditorJS}[] = [];
 
   constructor() {
   }
 
-  public setEditor(editor : EditorJS){
-    this.editorJS = editor;
+  public addToEditorList(id : number, editor : EditorJS) : void{
+
+    console.log("Adding new editor");
+
+    let actualEditor : {id: number, editor : EditorJS} = {
+      id: id,
+      editor : editor
+    }
+
+    this.editors.push(actualEditor);
+
   }
 
-  public saveContent() : Promise<OutputData> {
-    return this.editorJS.save()
+  public saveContent(id : number) : Promise<OutputData>{
+
+    let editorToSave  = this.editors.find((editor) => editor.id === id);
+
+    if (editorToSave?.editor){
+      return editorToSave.editor.save();
+    }
+
+    return Promise.reject("Editor error");
+
+  }
+
+  public editorExist(id : number) : boolean{
+    return this.editors.findIndex((editor) => editor.id == id) > -1;
+  }
+
+  public destroyEditor (id : number) {
+    let editorIndex = this.editors.findIndex((obj) => obj.id === id);
+    this.editors.splice(editorIndex, 1);
   }
 
 }
