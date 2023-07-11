043d09ed2834e068c6ceaa62183ca9f8933300d9
Joseph <lievanosan@gmail.com>
2023-07-12 01:33:58 +0200

    Main bsPriority component.

---
+++ src/app/Components/Features/Business/bsPriority/bs-priority/bs-priority.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bs-priority',
+  templateUrl: './bs-priority.component.html',
+  styleUrls: ['./bs-priority.component.css']
+})
+export class BsPriorityComponent {
+
+}
