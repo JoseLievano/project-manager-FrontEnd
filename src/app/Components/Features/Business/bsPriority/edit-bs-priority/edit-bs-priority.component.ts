6955fd3f222307004282a1782fecae3d39b45831
Joseph <lievanosan@gmail.com>
2023-07-13 19:05:01 +0200

    Component to edit a priority.

---
+++ src/app/Components/Features/Business/bsPriority/edit-bs-priority/edit-bs-priority.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-bs-priority',
+  templateUrl: './edit-bs-priority.component.html',
+  styleUrls: ['./edit-bs-priority.component.css']
+})
+export class EditBsPriorityComponent {
+
+}
