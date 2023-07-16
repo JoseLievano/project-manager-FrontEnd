ea209f837e2f1f99f52b9777901ecddc19f99c91
Joseph <lievanosan@gmail.com>
2023-07-16 18:52:32 +0200

    Component to add a new bs_client

---
+++ src/app/Components/Features/Business/bsClient/add-new-bs-client/add-new-bs-client.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-bs-client',
+  templateUrl: './add-new-bs-client.component.html',
+  styleUrls: ['./add-new-bs-client.component.css']
+})
+export class AddNewBsClientComponent {
+
+}
