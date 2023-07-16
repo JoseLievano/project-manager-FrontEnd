3994d333e00f9e9304bcea8ebcca34faf8c92be5
Joseph <lievanosan@gmail.com>
2023-07-16 18:52:56 +0200

    Main component for bs_clients.

---
+++ src/app/Components/Features/Business/bsClient/bs-client/bs-client.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bs-client',
+  templateUrl: './bs-client.component.html',
+  styleUrls: ['./bs-client.component.css']
+})
+export class BsClientComponent {
+
+}
