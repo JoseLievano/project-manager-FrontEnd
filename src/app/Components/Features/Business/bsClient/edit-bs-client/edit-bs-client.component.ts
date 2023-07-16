bb555eec2befe6636fee15c55b6d7f82b00fed89
Joseph <lievanosan@gmail.com>
2023-07-16 18:54:16 +0200

    Component to edit a client.

---
+++ src/app/Components/Features/Business/bsClient/edit-bs-client/edit-bs-client.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-bs-client',
+  templateUrl: './edit-bs-client.component.html',
+  styleUrls: ['./edit-bs-client.component.css']
+})
+export class EditBsClientComponent {
+
+}
