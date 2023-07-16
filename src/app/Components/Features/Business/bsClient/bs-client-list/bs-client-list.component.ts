a121aadb6669ca8caac8ab76905b5636438e15e4
Joseph <lievanosan@gmail.com>
2023-07-16 18:53:34 +0200

    Component to show the list of bs_clients.

---
+++ src/app/Components/Features/Business/bsClient/bs-client-list/bs-client-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bs-client-list',
+  templateUrl: './bs-client-list.component.html',
+  styleUrls: ['./bs-client-list.component.css']
+})
+export class BsClientListComponent {
+
+}
