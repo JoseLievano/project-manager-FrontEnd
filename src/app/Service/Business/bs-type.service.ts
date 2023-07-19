12593106e0fecc9e8dc135ee1d04cacfdd7fd94b
Joseph <lievanosan@gmail.com>
2023-07-20 01:29:42 +0200

    Override rolesAbleToAddNew and getButtonPermissions methods.

--- src/app/Service/Business/bs-type.service.ts
+++ src/app/Service/Business/bs-type.service.ts
@@ -5,6 +5,9 @@ import {HttpClient} from "@angular/common/http";
 import {LoginService} from "../Shared/login.service";
 import {Router} from "@angular/router";
 import {Const} from "../../Constant/const";
+import {ActionsButtons} from "../../Model/Shared/actions-buttons";
+import {tableActionButton} from "../../Constant/table-action-button";
+import {userRole} from "../../Constant/userRole";
 
 @Injectable({
   providedIn: 'root'
@@ -30,5 +33,20 @@ export class BsTypeService extends ModelService<bsType>{
       data.taskCategories,
       data.tasks
     );
+  }
+
+
+  protected override rolesAbleToAddNew(): string[] {
+    return [userRole.CLIENT, userRole.ADMIN];
+  }
+
+  public override getButtonPermissions(): ActionsButtons[] {
+    return [
+      {actionName: tableActionButton.LOAD, roles: [userRole.CLIENT]},
+      {actionName: tableActionButton.EDIT, roles: [userRole.CLIENT]},
+      {actionName: tableActionButton.DELETE, roles: [userRole.CLIENT]},
+      {actionName: tableActionButton.ADD, roles: [userRole.CLIENT]},
+      {actionName: tableActionButton.UPDATE, roles: [userRole.CLIENT]}
+    ];
   }
 }
