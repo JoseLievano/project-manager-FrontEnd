ac07379e34327a38bfad20cf2ab4acfe820fc5d3
Joseph <lievanosan@gmail.com>
2023-07-22 22:18:57 +0200

    Module to hold reusable form fields.

---
+++ src/app/Components/Shared/form-fields/form-fields.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NameFieldComponent } from './name-field/name-field.component';
+import { FirstNameFieldComponent } from './first-name-field/first-name-field.component';
+import {ReactiveFormsModule} from "@angular/forms";
+
+
+
+@NgModule({
+  declarations: [
+    NameFieldComponent,
+    FirstNameFieldComponent
+  ],
+  exports: [
+    NameFieldComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ]
+})
+export class FormFieldsModule { }
