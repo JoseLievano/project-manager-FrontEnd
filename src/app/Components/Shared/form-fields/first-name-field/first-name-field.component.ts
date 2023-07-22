177c01e2b38074b77baa62ff11b1e8cab5fe3ffd
Joseph <lievanosan@gmail.com>
2023-07-22 22:18:31 +0200

    Component to hold first name fields.

---
+++ src/app/Components/Shared/form-fields/first-name-field/first-name-field.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-name-field',
+  templateUrl: './first-name-field.component.html',
+  styleUrls: ['./first-name-field.component.css']
+})
+export class FirstNameFieldComponent {
+
+}
