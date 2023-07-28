326ea1c2ae6095bdbe42912b8f7f26f69c716929
Joseph <lievanosan@gmail.com>
2023-07-29 00:13:22 +0200

    Modify to use name field component.

--- src/app/Components/Features/Business/bsPriority/add-new-bs-priority/add-new-bs-priority.component.ts
+++ src/app/Components/Features/Business/bsPriority/add-new-bs-priority/add-new-bs-priority.component.ts
@@ -1,84 +1,74 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {BusinessService} from "../../../../../Service/Business/business.service";
-import {BsPriorityService} from "../../../../../Service/Business/bs-priority.service";
-import {bsPriority} from "../../../../../Model/Business/bsPriority";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {Subscription} from "rxjs";
-import {AlertService} from "../../../../../Service/Shared/alert.service";
-import {ErrorHandlerService} from "../../../../../Service/Shared/error-handler.service";
-import {UiMessage} from "../../../../../Model/Shared/ui-message";
-import {messageType} from "../../../../../Constant/messageType";
-import {ActionModelEmit} from "../../../../../Model/Shared/actionModelEmit";
-import {actionType} from "../../../../../Constant/actionType";
+import { Component, OnInit } from '@angular/core';
+import { BusinessService } from '../../../../../Service/Business/business.service';
+import { BsPriorityService } from '../../../../../Service/Business/bs-priority.service';
+import { bsPriority } from '../../../../../Model/Business/bsPriority';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AlertService } from '../../../../../Service/Shared/alert.service';
+import { ErrorHandlerService } from '../../../../../Service/Shared/error-handler.service';
+import { UiMessage } from '../../../../../Model/Shared/ui-message';
+import { messageType } from '../../../../../Constant/messageType';
+import { ActionModelEmit } from '../../../../../Model/Shared/actionModelEmit';
+import { actionType } from '../../../../../Constant/actionType';
 
 @Component({
-  selector: 'app-add-new-bs-priority',
-  templateUrl: './add-new-bs-priority.component.html',
-  styleUrls: ['./add-new-bs-priority.component.css']
+    selector: 'app-add-new-bs-priority',
+    templateUrl: './add-new-bs-priority.component.html',
+    styleUrls: ['./add-new-bs-priority.component.css'],
 })
-export class AddNewBsPriorityComponent implements OnInit, OnDestroy{
+export class AddNewBsPriorityComponent implements OnInit {
+    private newPriority: bsPriority = new bsPriority();
 
-  private newPriority : bsPriority = new bsPriority();
+    public model: bsPriority = new bsPriority();
 
-  public newPriorityForm : FormGroup = new FormGroup<any>({
-    name : new FormControl("", {validators : [Validators.required]})
-  });
+    public newPriorityForm: FormGroup = this.formBuilder.group({
+        name: [''],
+    });
 
-  public formChangeSub : Subscription;
+    constructor(
+        private businessService: BusinessService,
+        public bsPriorityService: BsPriorityService,
+        private alertService: AlertService,
+        private errorService: ErrorHandlerService,
+        private formBuilder: FormBuilder,
+    ) {}
 
-  public formHasValidName : boolean = false;
-
-  constructor(
-    private businessService : BusinessService,
-    private bsPriorityService : BsPriorityService,
-    private alertService : AlertService,
-    private errorService : ErrorHandlerService
-  ) {
-    this.formChangeSub = this.newPriorityForm.valueChanges.subscribe({
-      next : () => {
-        this.formHasValidName = !!(this.newPriorityForm.value.name.length >= 3 && this.newPriorityForm.value.name && this.newPriorityForm.value.name != "   ");
-      }
-    })
-  }
-
-  ngOnInit(): void {
-    this.newPriorityForm.controls["name"].setValue("");
-
-    //Set business of the new priority
-    this.newPriority.business = this.businessService.getLoadedBusiness();
-  }
-
-  public createNewPriority(){
-    if (this.formHasValidName){
-      this.newPriority.name = this.newPriorityForm.value.name;
-      let createNewBsPriority = this.bsPriorityService.createNew(this.newPriority).subscribe({
-        next : (response) => {
-          const bsPriorityReturned : bsPriority = response;
-          if (bsPriorityReturned.name && bsPriorityReturned.id){
-            this.alertService.addNewAlert(
-              new UiMessage("Priority " + bsPriorityReturned.name + " has been created", messageType.SUCCESS)
-            )
-            this.newPriorityForm.controls["name"].setValue("");
-            const emitModel : ActionModelEmit<bsPriority> = new ActionModelEmit<bsPriority>(actionType.NEW, bsPriorityReturned);
-            this.bsPriorityService.modelsChanged.emit(emitModel);
-          }
-        },
-        error : (err) => {
-          this.errorService.processError(err);
-        },
-        complete : () => {
-          createNewBsPriority.unsubscribe();
-        }
-      })
-    }else {
-      this.alertService.addNewAlert(
-        new UiMessage("You didn't add a valid name for this priority", messageType.ERROR)
-      )
+    ngOnInit(): void {
+        this.newPriorityForm.controls['name'].setValue('');
+        //Set business of the new priority
+        this.newPriority.business = this.businessService.getLoadedBusiness();
     }
-  }
 
-  ngOnDestroy(): void {
-    this.formChangeSub.unsubscribe();
-  }
-
+    public createNewPriority() {
+        this.newPriority.name = this.newPriorityForm.value.name;
+        let createNewBsPriority = this.bsPriorityService
+            .createNew(this.newPriority)
+            .subscribe({
+                next: (response) => {
+                    const bsPriorityReturned: bsPriority = response;
+                    if (bsPriorityReturned.name && bsPriorityReturned.id) {
+                        this.alertService.addNewAlert(
+                            new UiMessage(
+                                'Priority ' +
+                                    bsPriorityReturned.name +
+                                    ' has been created',
+                                messageType.SUCCESS,
+                            ),
+                        );
+                        this.newPriorityForm.controls['name'].setValue('');
+                        const emitModel: ActionModelEmit<bsPriority> =
+                            new ActionModelEmit<bsPriority>(
+                                actionType.NEW,
+                                bsPriorityReturned,
+                            );
+                        this.bsPriorityService.modelsChanged.emit(emitModel);
+                    }
+                },
+                error: (err) => {
+                    this.errorService.processError(err);
+                },
+                complete: () => {
+                    createNewBsPriority.unsubscribe();
+                },
+            });
+    }
 }
--- src/app/Components/Features/Business/bsTaskCategory/add-new-bs-task-category/add-new-bs-task-category.component.ts
+++ src/app/Components/Features/Business/bsTaskCategory/add-new-bs-task-category/add-new-bs-task-category.component.ts
@@ -1,58 +1,69 @@
 import { Component } from '@angular/core';
-import {BsTaskCategoryService} from "../../../../../Service/Business/bs-task-category.service";
-import {BusinessService} from "../../../../../Service/Business/business.service";
-import {bsTaskCategory} from "../../../../../Model/Business/bsTaskCategory";
-import {FormBuilder} from "@angular/forms";
-import {AlertService} from "../../../../../Service/Shared/alert.service";
-import {UiMessage} from "../../../../../Model/Shared/ui-message";
-import {messageType} from "../../../../../Constant/messageType";
-import {ErrorHandlerService} from "../../../../../Service/Shared/error-handler.service";
-import {ActionModelEmit} from "../../../../../Model/Shared/actionModelEmit";
-import {actionType} from "../../../../../Constant/actionType";
+import { BsTaskCategoryService } from '../../../../../Service/Business/bs-task-category.service';
+import { BusinessService } from '../../../../../Service/Business/business.service';
+import { bsTaskCategory } from '../../../../../Model/Business/bsTaskCategory';
+import { FormBuilder } from '@angular/forms';
+import { AlertService } from '../../../../../Service/Shared/alert.service';
+import { UiMessage } from '../../../../../Model/Shared/ui-message';
+import { messageType } from '../../../../../Constant/messageType';
+import { ErrorHandlerService } from '../../../../../Service/Shared/error-handler.service';
+import { ActionModelEmit } from '../../../../../Model/Shared/actionModelEmit';
+import { actionType } from '../../../../../Constant/actionType';
 
 @Component({
-  selector: 'app-add-new-bs-task-category',
-  templateUrl: './add-new-bs-task-category.component.html',
-  styleUrls: ['./add-new-bs-task-category.component.css']
+    selector: 'app-add-new-bs-task-category',
+    templateUrl: './add-new-bs-task-category.component.html',
+    styleUrls: ['./add-new-bs-task-category.component.css'],
 })
 export class AddNewBsTaskCategoryComponent {
+    private taskCategory: bsTaskCategory = new bsTaskCategory();
 
-  private taskCategory : bsTaskCategory = new bsTaskCategory();
+    public model: bsTaskCategory = new bsTaskCategory();
 
-  public newTaskCategoryForm = this.formBuilder.group({
-    name : [null]
-  });
+    public newTaskCategoryForm = this.formBuilder.group({
+        name: [null],
+    });
 
-  constructor(
-    private bsTaskCategoryService : BsTaskCategoryService,
-    private businessService : BusinessService,
-    private alertService : AlertService,
-    private errorService : ErrorHandlerService,
-    private formBuilder : FormBuilder
-  ) {
-    this.taskCategory.business = this.businessService.getLoadedBusiness();
-  }
+    constructor(
+        public bsTaskCategoryService: BsTaskCategoryService,
+        private businessService: BusinessService,
+        private alertService: AlertService,
+        private errorService: ErrorHandlerService,
+        private formBuilder: FormBuilder,
+    ) {
+        this.taskCategory.business = this.businessService.getLoadedBusiness();
+    }
 
-  public addNewTaskCategory(){
-    this.taskCategory.name = this.newTaskCategoryForm.value.name;
-    this.newTaskCategoryForm.controls.name.setValue(null);
-    let taskCatSubscription = this.bsTaskCategoryService.createNew<bsTaskCategory>(this.taskCategory).subscribe({
-      next : (response) => {
-        if (response.id){
-          const emitModel : ActionModelEmit<bsTaskCategory> = new ActionModelEmit<bsTaskCategory>(actionType.NEW, response);
-          this.bsTaskCategoryService.modelsChanged.emit(emitModel);
-        }
-        this.alertService.addNewAlert(
-          new UiMessage("New task category " + response.name + " added", messageType.SUCCESS)
-        )
-      },
-      error : err => {
-        this.errorService.processError(err);
-      },
-      complete : () =>{
-        taskCatSubscription.unsubscribe();
-      }
-    })
-  }
-
+    public addNewTaskCategory() {
+        this.taskCategory.name = this.newTaskCategoryForm.value.name;
+        this.newTaskCategoryForm.controls.name.setValue(null);
+        let taskCatSubscription = this.bsTaskCategoryService
+            .createNew<bsTaskCategory>(this.taskCategory)
+            .subscribe({
+                next: (response) => {
+                    if (response.id) {
+                        const emitModel: ActionModelEmit<bsTaskCategory> =
+                            new ActionModelEmit<bsTaskCategory>(
+                                actionType.NEW,
+                                response,
+                            );
+                        this.bsTaskCategoryService.modelsChanged.emit(
+                            emitModel,
+                        );
+                    }
+                    this.alertService.addNewAlert(
+                        new UiMessage(
+                            'New task category ' + response.name + ' added',
+                            messageType.SUCCESS,
+                        ),
+                    );
+                },
+                error: (err) => {
+                    this.errorService.processError(err);
+                },
+                complete: () => {
+                    taskCatSubscription.unsubscribe();
+                },
+            });
+    }
 }
