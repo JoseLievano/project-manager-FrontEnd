2fe9cffad529825248afcb5b8153fabbf5b4c9e2
Joseph <lievanosan@gmail.com>
2023-07-28 17:45:52 +0200

    Migrate login to CustomTextFieldValidator. Extends CustomTextFieldValidator

--- src/app/Components/Shared/form-fields/name-field/name-field.component.ts
+++ src/app/Components/Shared/form-fields/name-field/name-field.component.ts
@@ -1,139 +1,52 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {
-  AbstractControl,
-  ControlValueAccessor, NG_VALIDATORS,
-  NG_VALUE_ACCESSOR, ValidationErrors, Validator,
-} from "@angular/forms";
+    ControlValueAccessor,
+    NG_VALIDATORS,
+    NG_VALUE_ACCESSOR,
+    Validator,
+} from '@angular/forms';
+import { ModelService } from '../../../../Service/Shared/model.service';
+import { CustomTextFieldValidator } from '../../../../Util/customTextFieldValidator';
 
 @Component({
-  selector: 'app-name-field',
-  templateUrl: './name-field.component.html',
-  styleUrls: ['./name-field.component.css'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      multi: true,
-      useExisting: NameFieldComponent
-    },
-    {
-      provide: NG_VALIDATORS,
-      multi: true,
-      useExisting: NameFieldComponent
-    }
-  ]
+    selector: 'app-name-field',
+    templateUrl: './name-field.component.html',
+    styleUrls: ['./name-field.component.css'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            multi: true,
+            useExisting: NameFieldComponent,
+        },
+        {
+            provide: NG_VALIDATORS,
+            multi: true,
+            useExisting: NameFieldComponent,
+        },
+    ],
 })
-export class NameFieldComponent implements ControlValueAccessor, Validator{
+export class NameFieldComponent<T>
+    extends CustomTextFieldValidator<T>
+    implements ControlValueAccessor, Validator
+{
+    @Input() override label: string;
 
-  @Input() label: string;
+    @Input() override originalValue: any;
 
-  @Input() originalValue : any;
+    @Input() override isRequired: boolean = false;
 
-  public actualName : string = "";
+    @Input() override modelService: ModelService<T>;
 
-  public onChange: any = () => {};
+    @Input() override model: T;
 
-  public onTouched: any = () => {};
-
-  public touched : boolean = false;
-
-  public hasBeenModified : boolean = false;
-
-  public equalToOriginal : boolean = false;
-
-  public isEmpty : boolean = true;
-
-  public invalidUseOfSpace : boolean = true;
-
-  public disabled : boolean = false;
-
-  public onValidatorChange = () => {};
-
-  private nameField : HTMLInputElement;
-
-  public hasLessThanTwoCharacters : boolean = true;
-
-  // Generate a unique id using a random number
-  public id = 'name-field-' + Math.floor(Math.random() * Math.floor(10000000)) + "-" + Math.floor(Math.random() * Math.floor(1030000));
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
-    this.nameField = document.getElementById("name") as HTMLInputElement;
-    if (this.originalValue){
-      this.nameField.value = this.originalValue;
+    constructor() {
+        super();
     }
 
-  }
-
-  public fieldChangeDetected(event: any) {
-    if (!this.hasBeenModified)
-      this.hasBeenModified = true;
-    this.actualName = event.target.value;
-    this.onChange(event.target.value);
-    this.onValidatorChange();
-  }
-
-  writeValue(value: any): void {
-    if (this.originalValue){
-      this.nameField.value = this.originalValue;
-      this.actualName = this.originalValue;
-      this.touched = false;
-      this.hasBeenModified = false;
-    }else{
-      this.actualName = value;
-      this.nameField.value = this.actualName;
-      this.touched = false;
-      this.hasBeenModified = false;
+    ngOnInit(): void {
+        this.textField = document.getElementById('name') as HTMLInputElement;
+        if (this.originalValue) {
+            this.textField.value = this.originalValue;
+        }
     }
-  }
-
-  registerOnChange(fn: any): void {
-    this.onChange = fn;
-  }
-
-  registerOnTouched(fn: any): void {
-    this.onTouched = fn;
-  }
-
-  setDisabledState(isDisabled: boolean): void {
-    this.disabled = isDisabled;
-  }
-
-  setTouched() {
-    this.onTouched();
-    this.touched = true;
-  }
-
-  registerOnValidatorChange(fn: () => void) {
-    this.onValidatorChange = fn;
-  }
-
-  validate(control: AbstractControl): ValidationErrors | null {
-    //Check field conditions and modified fields that holds conditions state
-    this.setValidationFlags();
-    if (this.isEmpty || this.equalToOriginal || this.hasLessThanTwoCharacters || this.invalidUseOfSpace){
-      let errors : any = {};
-      errors["isEmpty"] = this.isEmpty;
-      errors["equalToOriginal"] = this.equalToOriginal;
-      errors["hasLessThanTwoCharacters"] = this.hasLessThanTwoCharacters;
-      errors["invalidUseOfSpace"] = this.invalidUseOfSpace;
-      return errors;
-    }
-    return null;
-  }
-
-  private setValidationFlags(){
-    if (this.actualName != null && this.actualName != ""){
-      this.isEmpty = false;
-      if (this.originalValue != null){
-        this.equalToOriginal = this.actualName == this.originalValue;
-      }
-      this.hasLessThanTwoCharacters = this.actualName.length < 3;
-      this.invalidUseOfSpace = /^\s/.test(this.actualName) || /^\s*$/.test(this.actualName) || /\s{2,}/.test(this.actualName);
-    }else{
-      this.isEmpty = true;
-    }
-  }
-
 }
