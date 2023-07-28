9d32e0dd6edb371d5d0070154b2baebc4ec08224
Joseph <lievanosan@gmail.com>
2023-07-29 00:11:42 +0200

    Modify for better readability.

--- src/app/Components/Shared/shared-components.module.ts
+++ src/app/Components/Shared/shared-components.module.ts
@@ -1,50 +1,48 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {ThemeSelectorComponent} from "./theme-selector/theme-selector.component";
+import { ThemeSelectorComponent } from './theme-selector/theme-selector.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import {RouterOutlet} from "@angular/router";
+import { RouterOutlet } from '@angular/router';
 import { LogoutBtnTopComponent } from './logout-btn-top/logout-btn-top.component';
 import { TableViewComponent } from './table-view/table-view.component';
 import { AlertsComponent } from './alerts/alerts.component';
 import { CategoryViewComponent } from './category-view/category-view.component';
 import { CategoryAddNewComponent } from './category-add-new/category-add-new.component';
-import {ReactiveFormsModule} from "@angular/forms";
-import {ContentModule} from "./content/content.module";
-import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
-import {FormFieldsModule} from "./form-fields/form-fields.module";
-
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { ContentModule } from './content/content.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormFieldsModule } from './form-fields/form-fields.module';
 
 @NgModule({
-  declarations: [
-    ThemeSelectorComponent,
-    SidebarComponent,
-    TopBarComponent,
-    DashboardComponent,
-    LogoutBtnTopComponent,
-    TableViewComponent,
-    AlertsComponent,
-    CategoryViewComponent,
-    CategoryAddNewComponent
-  ],
-  imports: [
-    ContentModule,
-    CommonModule,
-    RouterOutlet,
-    ReactiveFormsModule,
-    FontAwesomeModule,
-    FormFieldsModule
-  ],
-  exports: [
-    TopBarComponent,
-    ThemeSelectorComponent,
-    SidebarComponent,
-    TableViewComponent,
-    AlertsComponent,
-    CategoryViewComponent,
-    CategoryAddNewComponent,
-  ]
+    declarations: [
+        ThemeSelectorComponent,
+        SidebarComponent,
+        TopBarComponent,
+        DashboardComponent,
+        LogoutBtnTopComponent,
+        TableViewComponent,
+        AlertsComponent,
+        CategoryViewComponent,
+        CategoryAddNewComponent,
+    ],
+    imports: [
+        ContentModule,
+        CommonModule,
+        RouterOutlet,
+        ReactiveFormsModule,
+        FontAwesomeModule,
+        FormFieldsModule,
+    ],
+    exports: [
+        TopBarComponent,
+        ThemeSelectorComponent,
+        SidebarComponent,
+        TableViewComponent,
+        AlertsComponent,
+        CategoryViewComponent,
+        CategoryAddNewComponent,
+    ],
 })
-export class SharedComponentsModule { }
+export class SharedComponentsModule {}
