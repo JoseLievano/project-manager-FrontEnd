27fbc5b9af12da68ed0404c1f56f0a6f7d1c0c72
Joseph <lievanosan@gmail.com>
2023-08-03 23:32:21 +0200

    Eliminate child routes.

--- src/app/Components/Features/Project/project/project.module.ts
+++ src/app/Components/Features/Project/project/project.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedComponentsModule } from '../../../Shared/shared-components.module';
-import { RouterModule, RouterOutlet, Routes } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { FormFieldsModule } from '../../../Shared/form-fields/form-fields.module';
 import { ProjectComponent } from './project/project.component';
 import { ProjectListComponent } from './project-list/project-list.component';
-
-const routes: Routes = [{ path: '', component: ProjectListComponent }];
+import { AddNewProjectComponent } from './add-new-project/add-new-project.component';
 
 @NgModule({
-    declarations: [ProjectComponent, ProjectListComponent],
+    declarations: [ProjectComponent, ProjectListComponent, AddNewProjectComponent],
     imports: [
         CommonModule,
         SharedComponentsModule,
         FormFieldsModule,
         RouterOutlet,
-        RouterModule.forChild(routes),
     ],
 })
 export class ProjectModule {}
