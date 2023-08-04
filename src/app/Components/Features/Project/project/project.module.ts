0e10f2336489e15da94e4b84709817c0bd6c00c3
Joseph <lievanosan@gmail.com>
2023-08-04 13:15:35 +0200

    Add Reactive Forms Module.

--- src/app/Components/Features/Project/project/project.module.ts
+++ src/app/Components/Features/Project/project/project.module.ts
@@ -6,14 +6,20 @@ import { FormFieldsModule } from '../../../Shared/form-fields/form-fields.module
 import { ProjectComponent } from './project/project.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { AddNewProjectComponent } from './add-new-project/add-new-project.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-    declarations: [ProjectComponent, ProjectListComponent, AddNewProjectComponent],
+    declarations: [
+        ProjectComponent,
+        ProjectListComponent,
+        AddNewProjectComponent,
+    ],
     imports: [
         CommonModule,
         SharedComponentsModule,
         FormFieldsModule,
         RouterOutlet,
+        ReactiveFormsModule,
     ],
 })
 export class ProjectModule {}
