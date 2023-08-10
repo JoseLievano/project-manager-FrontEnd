826b68a5b6621a95125f8261e191d00a61629ce1
Joseph <lievanosan@gmail.com>
2023-08-10 11:43:56 +0200

    Added additional dependencies and a new DateFieldComponent to the FormFieldsModule.

--- src/app/Components/Shared/form-fields/form-fields.module.ts
+++ src/app/Components/Shared/form-fields/form-fields.module.ts
@@ -11,6 +11,12 @@ import { CompanyNameFieldComponent } from './company-name-field/company-name-fie
 import { EmailFieldComponent } from './email-field/email-field.component';
 import { PasswordFieldComponent } from './password-field/password-field.component';
 import { BsClientFieldComponent } from './bs-client-field/bs-client-field.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { DateFieldComponent } from './date-field/date-field.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
     declarations: [
@@ -24,6 +30,7 @@ import { BsClientFieldComponent } from './bs-client-field/bs-client-field.compon
         EmailFieldComponent,
         PasswordFieldComponent,
         BsClientFieldComponent,
+        DateFieldComponent,
     ],
     exports: [
         NameFieldComponent,
@@ -36,7 +43,16 @@ import { BsClientFieldComponent } from './bs-client-field/bs-client-field.compon
         EmailFieldComponent,
         PasswordFieldComponent,
         BsClientFieldComponent,
+        DateFieldComponent,
     ],
-    imports: [CommonModule, ReactiveFormsModule],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        FontAwesomeModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatNativeDateModule,
+    ],
 })
 export class FormFieldsModule {}
