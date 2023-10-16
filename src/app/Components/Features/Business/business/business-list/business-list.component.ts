751a8b436aae9dac57245436087ad847a2347f6f
Joseph <lievanosan@gmail.com>
2023-10-16 13:59:29 +0200

    Formatted

--- src/app/Components/Features/Business/business/business-list/business-list.component.ts
+++ src/app/Components/Features/Business/business/business-list/business-list.component.ts
@@ -1,30 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import {BusinessService} from "../../../../../Service/Business/business.service";
-import {Business} from "../../../../../Model/Business/Business";
-import {LoginService} from "../../../../../Service/Shared/login.service";
-import {User} from "../../../../../Model/Shared/User";
+import { BusinessService } from '../../../../../Service/Business/business.service';
+import { Business } from '../../../../../Model/Business/Business';
+import { LoginService } from '../../../../../Service/Shared/login.service';
+import { User } from '../../../../../Model/Shared/User';
 
 @Component({
-  selector: 'app-business-list',
-  templateUrl: './business-list.component.html',
-  styleUrls: ['./business-list.component.css']
+    selector: 'app-business-list',
+    templateUrl: './business-list.component.html',
+    styleUrls: ['./business-list.component.css'],
 })
 export class BusinessListComponent implements OnInit {
+    public model: Business = new Business();
 
-  public model : Business = new Business();
+    private user: User | null;
 
-  private user : User | null;
+    constructor(
+        public businessService: BusinessService,
+        private loginService: LoginService,
+    ) {
+        let userId: Number | null | undefined =
+            loginService.getActualUser()?.id;
 
-  constructor(public businessService : BusinessService,
-              private loginService : LoginService) {
+        this.user = loginService.getActualUser();
+    }
 
-    let userId : Number | null | undefined = loginService.getActualUser()?.id;
+    ngOnInit(): void {}
 
-    this.user = loginService.getActualUser();
-  }
-
-  ngOnInit(): void {
-  }
-
-  protected readonly Business = Business;
+    protected readonly Business = Business;
 }
