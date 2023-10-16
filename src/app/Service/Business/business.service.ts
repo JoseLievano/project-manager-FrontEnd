9b50f6093aa1f67324cbf6c094397b522b011520
Joseph <lievanosan@gmail.com>
2023-10-16 13:59:14 +0200

    Formatted

--- src/app/Service/Business/business.service.ts
+++ src/app/Service/Business/business.service.ts
@@ -1,143 +1,215 @@
-import {Injectable} from '@angular/core';
-import {Const} from "../../Constant/const";
-import {HttpClient} from "@angular/common/http";
-import {ModelService} from "../Shared/model.service";
-import {Business} from "../../Model/Business/Business";
-import {LoginService} from "../Shared/login.service";
-import {PageRequest} from "../../Model/Shared/pageRequest";
-import {ActionsButtons} from "../../Model/Shared/actions-buttons";
-import {tableActionButton} from "../../Constant/table-action-button";
-import {Router} from "@angular/router";
-import {userRole} from "../../Constant/userRole";
-import {ErrorHandlerService} from "../Shared/error-handler.service";
-import {errorMessages} from "../../Constant/errorMessages";
-import {ViewKey, ViewKeyBuilder} from "../../Model/Shared/ViewKey";
-import {FilterRequest} from "../../Model/Shared/filterRequest";
-import {OperationRequest} from "../../Model/Shared/operationRequest";
+import { Injectable } from '@angular/core';
+import { Const } from '../../Constant/const';
+import { HttpClient } from '@angular/common/http';
+import { ModelService } from '../Shared/model.service';
+import { Business } from '../../Model/Business/Business';
+import { LoginService } from '../Shared/login.service';
+import { PageRequest } from '../../Model/Shared/pageRequest';
+import { ActionsButtons } from '../../Model/Shared/actions-buttons';
+import { tableActionButton } from '../../Constant/table-action-button';
+import { Router } from '@angular/router';
+import { userRole } from '../../Constant/userRole';
+import { ErrorHandlerService } from '../Shared/error-handler.service';
+import { errorMessages } from '../../Constant/errorMessages';
+import { ViewKey, ViewKeyBuilder } from '../../Model/Shared/ViewKey';
+import { FilterRequest } from '../../Model/Shared/filterRequest';
+import { OperationRequest } from '../../Model/Shared/operationRequest';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class BusinessService extends ModelService<Business> {
+    private url: String;
 
-  private url: String;
-
-  constructor(protected override http: HttpClient,
-              protected override loginService: LoginService,
-              protected override router : Router,
-              private errorService : ErrorHandlerService) {
-    super(http,loginService, Const.API_URL + Const.BUSINESS, router);
-    this.url = Const.API_URL + Const.BUSINESS;
-  }
-
-  public createInstance(data: any): Business {
-    return new Business();
-  }
-
-  protected override pageListViewRestrictions(pageRequest: PageRequest) {
-
-    if (this.actualUser != null || this.actualUser != undefined) {
-      // @ts-ignore
-      if (this.actualUser.roles[0] != userRole.CLIENT) {
-        let newError : Error = new Error();
-        newError.message = errorMessages.FORBIDDEN;
-        newError.name = "Internal Error";
-        this.errorService.processError(newError);
-      }
+    constructor(
+        protected override http: HttpClient,
+        protected override loginService: LoginService,
+        protected override router: Router,
+        private errorService: ErrorHandlerService,
+    ) {
+        super(http, loginService, Const.API_URL + Const.BUSINESS, router);
+        this.url = Const.API_URL + Const.BUSINESS;
     }
 
-    if (pageRequest.sort?.length == 0) {
-      pageRequest.sort = [
-        {"property": "id", "isAscending": true}
-      ];
+    public createInstance(data: any): Business {
+        return new Business();
     }
-  }
 
-  override getButtonPermissions(): ActionsButtons[] {
-    return [
-      {actionName: tableActionButton.LOAD, roles: [userRole.CLIENT]},
-      {actionName: tableActionButton.EDIT, roles: [userRole.CLIENT]},
-      {actionName: tableActionButton.DELETE, roles: [userRole.CLIENT]},
-      {actionName: tableActionButton.ADD, roles: [userRole.CLIENT]},
-      {actionName: tableActionButton.UPDATE, roles: [userRole.CLIENT]}
-    ];
-  }
+    protected override pageListViewRestrictions(pageRequest: PageRequest) {
+        if (this.actualUser != null || this.actualUser != undefined) {
+            /*console.log('Actual role', this.actualUser);*/
+            // @ts-ignore
+            if (this.actualUser.roles[0] != userRole.CLIENT) {
+                let newError: Error = new Error();
+                newError.message = errorMessages.FORBIDDEN;
+                newError.name = 'Internal Error';
+                this.errorService.processError(newError);
+            }
+        }
 
-  override executeAction(action: string, id: number): void {
-    switch (action) {
-      case tableActionButton.LOAD:
-        this.loadBusiness(id);
-        break;
+        if (pageRequest.sort?.length == 0) {
+            pageRequest.sort = [{ property: 'id', isAscending: true }];
+        }
     }
-  }
 
-  //Methods to load and unload a business in the local storage
-  public loadBusiness(businessID: number) {
-    window.localStorage.setItem("business", JSON.stringify(businessID));
-    this.router.navigate(["dashboard"])
-  }
-
-  public unloadBusiness() {
-    window.localStorage.setItem("business", "-1");
-    this.router.navigate(["dashboard"]);
-  }
-
-  public getLoadedBusiness(): number {
-    let actualBusiness: number = JSON.parse(<string>window.localStorage.getItem("business"));
-    if (actualBusiness >= 0) {
-      return actualBusiness;
+    override getButtonPermissions(): ActionsButtons[] {
+        return [
+            { actionName: tableActionButton.LOAD, roles: [userRole.CLIENT] },
+            { actionName: tableActionButton.EDIT, roles: [userRole.CLIENT] },
+            { actionName: tableActionButton.DELETE, roles: [userRole.CLIENT] },
+            { actionName: tableActionButton.ADD, roles: [userRole.CLIENT] },
+            { actionName: tableActionButton.UPDATE, roles: [userRole.CLIENT] },
+        ];
     }
-    return -1;
-  }
 
-  protected override rolesAbleToAddNew(): string[] {
-    return [userRole.CLIENT, userRole.ADMIN];
-  }
+    override executeAction(action: string, id: number): void {
+        switch (action) {
+            case tableActionButton.LOAD:
+                this.loadBusiness(id);
+                break;
+        }
+    }
 
-  protected override getViewKeys(): ViewKey[] {
+    //Methods to load and unload a business in the local storage
+    public loadBusiness(businessID: number) {
+        window.localStorage.setItem('business', JSON.stringify(businessID));
+        this.router.navigate(['dashboard']);
+    }
 
-    let commonRoles : string[] = [userRole.ADMIN, userRole.CLIENT, userRole.EMPLOYEE];
+    public unloadBusiness() {
+        window.localStorage.setItem('business', '-1');
+        this.router.navigate(['dashboard']);
+    }
 
-    return [
-      ViewKeyBuilder.builder().setPrivateKeyName("id").setPublicKeyName("ID").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("name").setPublicKeyName("Name").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("taxID").setPublicKeyName("Tax ID").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("dateCreated").setPublicKeyName("Date").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("pendingInvoice").setPublicKeyName("Pending Invoice").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("overDue").setPublicKeyName("Overdue").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("isActive").setPublicKeyName("Active").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("client").setPublicKeyName("Client").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("invoices").setPublicKeyName("Invoices").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("plan").setPublicKeyName("Plan").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("bsClients").setPublicKeyName("Clients").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("bsDocs").setPublicKeyName("Docs").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("bsDocsCategories").setPublicKeyName("Docs Categories").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("bsEmployees").setPublicKeyName("Employees").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("bsInvoices").setPublicKeyName("Customer Invoices").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("bsKBCategories").setPublicKeyName("KB Categories").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("bsKBs").setPublicKeyName("KBs").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("bsManagers").setPublicKeyName("Managers").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("bsPriorities").setPublicKeyName("Priorities").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("bsStatuses").setPublicKeyName("Statuses").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("bsTaskCategories").setPublicKeyName("Task Categories").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("bsTypes").setPublicKeyName("Types").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("bsProjects").setPublicKeyName("Projects").setAccessRole(commonRoles),
-      ViewKeyBuilder.builder().setPrivateKeyName("bsPrTasks").setPublicKeyName("Tasks").setAccessRole(commonRoles)
-    ];
+    public getLoadedBusiness(): number {
+        let actualBusiness: number = JSON.parse(
+            <string>window.localStorage.getItem('business'),
+        );
+        if (actualBusiness >= 0) {
+            return actualBusiness;
+        }
+        return -1;
+    }
 
-  }
+    protected override rolesAbleToAddNew(): string[] {
+        return [userRole.CLIENT, userRole.ADMIN];
+    }
 
-  public filterReqWithLoadedBusiness() : FilterRequest{
+    protected override getViewKeys(): ViewKey[] {
+        let commonRoles: string[] = [
+            userRole.ADMIN,
+            userRole.CLIENT,
+            userRole.EMPLOYEE,
+        ];
 
-    const filterReq : FilterRequest = new FilterRequest();
-    filterReq.field = "business";
-    filterReq.operations = [new OperationRequest()];
-    filterReq.operations[0].field = "id";
-    filterReq.operations[0].operator = "=";
-    filterReq.operations[0].value = this.getLoadedBusiness().toString();
+        return [
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('id')
+                .setPublicKeyName('ID')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('name')
+                .setPublicKeyName('Name')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('taxID')
+                .setPublicKeyName('Tax ID')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('dateCreated')
+                .setPublicKeyName('Date')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('pendingInvoice')
+                .setPublicKeyName('Pending Invoice')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('overDue')
+                .setPublicKeyName('Overdue')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('isActive')
+                .setPublicKeyName('Active')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('client')
+                .setPublicKeyName('Client')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('invoices')
+                .setPublicKeyName('Invoices')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('plan')
+                .setPublicKeyName('Plan')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('bsClients')
+                .setPublicKeyName('Clients')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('bsDocs')
+                .setPublicKeyName('Docs')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('bsDocsCategories')
+                .setPublicKeyName('Docs Categories')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('bsEmployees')
+                .setPublicKeyName('Employees')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('bsInvoices')
+                .setPublicKeyName('Customer Invoices')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('bsKBCategories')
+                .setPublicKeyName('KB Categories')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('bsKBs')
+                .setPublicKeyName('KBs')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('bsManagers')
+                .setPublicKeyName('Managers')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('bsPriorities')
+                .setPublicKeyName('Priorities')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('bsStatuses')
+                .setPublicKeyName('Statuses')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('bsTaskCategories')
+                .setPublicKeyName('Task Categories')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('bsTypes')
+                .setPublicKeyName('Types')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('bsProjects')
+                .setPublicKeyName('Projects')
+                .setAccessRole(commonRoles),
+            ViewKeyBuilder.builder()
+                .setPrivateKeyName('bsPrTasks')
+                .setPublicKeyName('Tasks')
+                .setAccessRole(commonRoles),
+        ];
+    }
 
-    return filterReq;
+    public filterReqWithLoadedBusiness(): FilterRequest {
+        const filterReq: FilterRequest = new FilterRequest();
+        filterReq.field = 'business';
+        filterReq.operations = [new OperationRequest()];
+        filterReq.operations[0].field = 'id';
+        filterReq.operations[0].operator = '=';
+        filterReq.operations[0].value = this.getLoadedBusiness().toString();
 
-  }
-
+        return filterReq;
+    }
 }
